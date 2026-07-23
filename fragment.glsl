9a85df95aadf38792facd6a159017a1ee5bfe1ae
DevinBayly <baylyd@arizona.edu>
2026-07-22 19:43:45 -0700

    pushconstant also available to fragment

--- fragment.glsl
+++ fragment.glsl
@@ -1,8 +1,11 @@
 #[fragment]
 #version 450
+layout(push_constant) uniform PushConstants {
+	float frame;
+} push_constants;
 
 layout (location = 0) out vec4 OutColor;
 
 void main() {
-	OutColor = vec4(1, 0.5, 0, 1);
+	OutColor = vec4(1, sin(push_constants.frame*.05), cos(push_constants.frame*.02), 1);
 }
